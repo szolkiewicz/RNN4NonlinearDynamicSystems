834bfc83cb938737522689fe19437c8921c9fc4c
Piotr Kulczycki <piotr9833@gmail.com>
2024-05-03 20:46:16 +0200

    fixed some bugs,  now u can see haow to use data generator from dataGeneration file

--- dataGeneration.py
+++ dataGeneration.py
@@ -1,35 +1,6 @@
 import math as m
 import enum
-
-class ModelType(enum.Enum):
-    SingleInputSingleOutput1 = 1
-    SingleInputSingleOutput2 = 2
-    TwoInputTwoOutput = 3
-
-def generate_data(length:int,data_size:int,Control_complexity:bool,model_type: ModelType):
-    if length <= 2:
-        return
-    Y = []
-    U = []
-    AppendInitialControl(U,data_size,Control_complexity)
-    if data_size == 2:
-        Y = [[],[]]
-        U = [[],[]]
-        if(model_type.value != 3):
-            return
-    Y = AppendInitialSystem(Y,U,model_type,data_size)
-
-    for k in range(length - 2):
-        #Generate U
-        AppendControl(U,data_size,Control_complexity,k)
-        #Generate Y
-        AppendSystem(Y,U,model_type)
-
-
-    print(Y)
-    print(U)
-    return [Y,U]
-
+import matplotlib.pyplot as plt
 
 #
 #       Control GENERATION
@@ -63,56 +34,25 @@ def AppendInitialControl(u,data_size:int,complexity:bool):
             TwoImputSimpleSin(u,-1)
 
 def SingleInputSimpleSin(u,k):
-    u.append(m.sin(m.pi * k /10))
+    u.append(2 * m.sin(m.pi * k /10))
 
 def SingleInputComplexSin(u,k):
-    u.append(3 * m.sin(m.pi * k /5) + 2 * m.cos(m.pi * k / 10))
+    u.append(1.75 * m.sin(m.pi * k /5) + 1.5 * m.cos(m.pi * k / 10))
 
 
 
 def TwoImputSimpleSin(u,k):
-    u[0].append(m.sin(m.pi * k /10))
-    u[1].append(m.cos(m.pi * k /10))
+    u[0].append(2 * m.sin(m.pi * k /10))
+    u[1].append(2 * m.cos(m.pi * k /10))
 
 
 def TwoImputComplexSin(u,k):
-    u[0].append(2 * m.cos(m.pi * k /5) + m.sin(m.pi * k /10))
-    u[1].append(2 * m.sin(m.pi * k /5) + m.cos(m.pi * k /10))
+    u[0].append(1.75 * m.cos(m.pi * k /5) + 1.5 * m.sin(m.pi * k /10))
+    u[1].append(1.75 * m.sin(m.pi * k /5) + 1.5 * m.cos(m.pi * k /10))
 
 #
 #       System GENERATION
 #
-def AppendSystem(Y,U,model:ModelType):
-    if model.value == 1:
-        SISO1(Y,U)
-    if model.value == 2:
-        SISO2(Y,U)
-    if model.value == 3:
-        TITO(Y,U)
-
-def AppendInitialSystem(Y,U,model:ModelType,data_size:int):
-    if data_size == 1:
-        Y.append(0)
-        Y.append(0)
-    if data_size == 2:
-        Y[0].append(0)
-        Y[0].append(0)
-        Y[1].append(0)
-        Y[1].append(0)
-    
-    if model.value == 1:
-        SISO1(Y,U)
-        SISO1(Y,U)
-    if model.value == 2:
-        SISO2(Y,U)
-        SISO2(Y,U)
-    if model.value == 3:
-        TITO(Y,U)
-        TITO(Y,U)
-    
-    return Y[2:len(Y)]
-
-
 def SISO1(Y,u):
     Y.append((Y[-1])/(pow(2,Y[-1]) + 1) + pow(u[-1],3)/(pow(Y[-1],2) + 1) - u[-1])
 
@@ -125,7 +65,81 @@ def TITO(Y,U):
     Y[0].append( (y1[-1])          / (1 + pow(y2[-1],2)) + U[0][-1] )
     Y[1].append( (y1[-1] * y2[-1]) / (1 + pow(y2[-1],2)) + U[1][-1] )
 
-data = generate_data(10,1,False,ModelType.SingleInputSingleOutput1)
+class ModelType(enum.Enum):
+    SingleInputSingleOutput1 = SISO1
+    SingleInputSingleOutput2 = SISO2
+    TwoInputTwoOutput = TITO
+
+def AppendSystem(Y,U,model:ModelType):
+    model(Y,U)
+
+def AppendInitialSystem(Y,U,model:ModelType,data_size:int):
+    if data_size == 1:
+        Y.append(0)
+        Y.append(0)
+    if data_size == 2:
+        Y[0].append(0)
+        Y[0].append(0)
+        Y[1].append(0)
+        Y[1].append(0)
+    
+    model(Y,U)
+    model(Y,U)
+
+    if data_size == 2:
+        Y = [Y[0][2:len(Y)],Y[1][2:len(Y)]]
+        return
+    
+    Y = Y[2:len(Y)]
+
+
+def generate_data(length:int,Control_complexity:bool,model_type: ModelType):
+    """function generate data of given model_type, length and input_complexity
+
+    Args:
+        length (int): hryzon na jaki generujemy dane
+        Control_complexity (bool): wybór wartości dotyczących sterowania na razie używamy sterowania typu sinusoidalnego, bez załkuceń o 2 złorzonościach 
+        model_type (ModelType): wybór typu modelu jaki zostanie użyty do generowania danych 
+
+    Returns:
+        _type_: zip złorzony z par (Y_n,U_n)
+    """
+    if length <= 2:
+        return
+    if model_type == ModelType.TwoInputTwoOutput:
+        data_size = 2
+    else:
+        data_size = 1
+    Y = []
+    U = []
+    if data_size == 2:
+        Y = [[],[]]
+        U = [[],[]]
+    AppendInitialControl(U,data_size,Control_complexity)
+    AppendInitialSystem(Y,U,model_type,data_size)
+
+    for k in range(length - 2):
+        #Generate U
+        AppendControl(U,data_size,Control_complexity,k)
+        #Generate Y
+        AppendSystem(Y,U,model_type)
+
+    if data_size == 2:
+        zip(zip(Y[0], U[0]),zip(Y[1], U[1]))
+    return zip(Y,U)
+
+
+data = generate_data(1000,True,ModelType.SingleInputSingleOutput1)
 print("")
+print("Wyświetlamy wygenerowane dane:")
+print("niebiseki - model   output")
+print("czerw     - control output")
 print("")
-print(data)
+unzippedData = []
+unzippedControl = []
+for elem in tuple(data): 
+    unzippedData.append(elem[0])
+    unzippedControl.append(elem[1])
+plt.plot(unzippedData,c='b')
+plt.plot(unzippedControl, c='r')
+plt.show()
