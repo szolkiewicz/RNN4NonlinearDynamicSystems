3dfc10646970ba9b5195d4c868fdf27e515c7bf7
Jacek Czyz <259265@student.pwr.edu.pl>
2024-05-26 18:13:26 +0200

    fixes, WORK IN PROGRESS, DO NOT MERGE

--- classy.py
+++ classy.py
@@ -1,32 +1,27 @@
 import torch.nn as nn
 import torch
 
-class AirModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.lstm = nn.LSTM(input_size=1, hidden_size=50, num_layers=1, batch_first=True)
-        self.linear = nn.Linear(50, 1)
-    def forward(self, x):
-        x, _ = self.lstm(x)
-        x = self.linear(x)
-        return x
-    
 
 class LSTMModel(nn.Module):
-    def __init__(self,input_size,hidden_size_1,hidden_size_2,out_size):
+    def __init__(self,input_size, hidden_size, out_size, num_layers):
         super().__init__()
-        self.hidden_size_1 = hidden_size_1
-        self.hidden_size_2 = hidden_size_2
+        self.hidden_size = hidden_size
         self.input_size = input_size
-        self.lstm_1 = nn.LSTM(input_size,hidden_size_1)
-        self.lstm_2 = nn.LSTM(hidden_size_1,hidden_size_2)
-        self.linear = nn.Linear(hidden_size_2,out_size)
-        self.hidden_1 = (torch.zeros(1,1,hidden_size_1), torch.zeros(1,1,hidden_size_1))
-        self.hidden_2 = (torch.zeros(1,1,hidden_size_2), torch.zeros(1,1,hidden_size_2))
+        self.num_layers=num_layers
+        self.lstm_1 = nn.LSTM(input_size,hidden_size, num_layers=self.num_layers)
+        self.linear = nn.Linear(hidden_size,out_size)
+        # Initialize hidden states
+        self.hidden = self.init_hidden(batch_size=1)
+
+    def init_hidden(self, batch_size):
+        # Create initial hidden and cell states for each layer
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+        return (h0, c0)
+
+    def forward(self, seq, hidden):
+        lstm_out_1, self.hidden = self.lstm_1(seq.view(len(seq), -1, self.input_size), hidden)
         
-    def forward(self,seq):
-        lstm_out_1 , self.hidden_1 = self.lstm_1(seq.view(-1,1,self.input_size),self.hidden_1)
-        lstm_out_2 , self.hidden_2 = self.lstm_2(lstm_out_1,self.hidden_2)
-        pred = self.linear(lstm_out_2.view(len(seq),-1))
+        pred = self.linear(lstm_out_1.view(len(seq), -1))
+        
         return pred
-    
